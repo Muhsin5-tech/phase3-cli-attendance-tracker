1ccc87b92a6e96999e1bd2d599d5a99d423a4d4f
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-12-18 11:10:26 +0100

    Functionality for recording attenance

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../../")))
 
-
+import datetime
 from lib.helper.seed_data import seed_data
 from lib.models.student import Student
 from lib.models.attendance import Attendance
@@ -55,7 +55,38 @@ def add_new_student():
         print(f"Student '{name}' added successfully!")
 
 def record_new_student():
-    pass 
+    print("\n--- Record Attendance ---")
+    students = session.query(Student).all()
+    if not students:
+        print("No student found, please add a student first.")
+        return
+    
+    print("\nSelect a student: ")
+    for student in students:
+        print(f"{student.id}. {student.name}. (Age: {student.age})")
+
+    student_id = input("Enter student ID: ").strip()
+    student = session.query(Student).filter_by(id=student_id).first()
+    if not student:
+        print("Invalid student ID, try again")
+        return
+    
+    date_input = input("Enter the date (YYYY-MM-DD): ").strip()
+    try:
+        date = datetime.datetime.strptime(date_input, "%Y-%m-%d").date()
+    except ValueError:
+        print("Invalid date format, please use YYYY-MM-DD.")
+        return
+    
+    status = input("Enter attendance status (present/absent): ")
+    if status not in ["present", "absent"]:
+        print("Invalid input, please use 'present' or 'absent'.")
+        return
+    
+    new_attendance = Attendance(student_id=student.id, date=date, status=status)
+    session.add(new_attendance)
+    session.commit()
+    print(f"Attendance recorded for {student.name} on {date} as {status}.")
 
 def view_student_attendance():
     pass
